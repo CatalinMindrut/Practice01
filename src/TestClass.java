aa194065e2f2a68417e77fd8d270761a878d261e
Catalin Mindrut <catalinm@APPELLO.OFFICE>
2018-06-25 14:25:34 +0300

    This is my second commit, just a siple comment.

--- src/TestClass.java
+++ src/TestClass.java
@@ -2,5 +2,6 @@ public class TestClass {
     public static void main(String[] args) {
         int testgit = 2;
         System.out.println(testgit);
+        //This is a practice comment, ignore it please. I will get good...er.
     }
 }
